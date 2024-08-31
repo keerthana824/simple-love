8701896795350744eaf6a1b3c9e4e58023e38a40
keerthana824 <146358624+keerthana824@users.noreply.github.com>
2024-08-31 17:44:06 +0530

    Add files via upload

---
+++ SANJU/script.js
@@ -0,0 +1,27 @@
+function goToNextPage() {
+    window.location.href = "page2.html";
+}
+
+function goToPage3() {
+    window.location.href = "page3.html";
+}
+
+function goToPage4() {
+    window.location.href = "page4.html";
+}
+
+function goToPage5(){
+    window.location.href= "page5.html";
+}
+
+function goToPage6(){
+    window.location.href= "page6.html";
+}
+
+function goToPage7(){
+    window.location.href= "page7.html";
+}
+
+function goToPage9(){
+    window.location.href="page9.html";
+}
